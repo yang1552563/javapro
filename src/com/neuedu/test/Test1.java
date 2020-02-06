e1c6900e39010cc4f451744305151520463fdd57
yang1552563 <57163285+yang1552563@users.noreply.github.com>
2020-02-06 17:54:08 +0800

    test1

---
+++ src/com/neuedu/test/Test1.java
@@ -0,0 +1,13 @@
+package com.neuedu.test;
+
+/**
+ * @author bin
+ * @date 2020/2/6 0006 17:50
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
